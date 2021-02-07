e6abc98bb72589e76638e57a1543269e78dc0d6d
Jules SANGLIER <jules.sanglier@gmail.com>
2021-02-07 23:07:41 +0100

    rangement /sql

--- adherent.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO bibliotheque2.adherent (nomAdh,prenomAdh,rueAdh,villeAdh,cpAdh,mailAdh) VALUES
-	 ('Sanglier','Jules','5 rue de la haute maison','St Pryvé St Mesmin',45750,'jules.sanglier@protonmail.com');
---
+++ sql/adherent.sql
@@ -0,0 +1,2 @@
+INSERT INTO bibliotheque2.adherent (nomAdh,prenomAdh,rueAdh,villeAdh,cpAdh,mailAdh) VALUES
+	 ('Sanglier','Jules','5 rue du bois jolie','Olivet',45160,'jules.sanglier@protonmail.com');
