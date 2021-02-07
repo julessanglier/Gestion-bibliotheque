de10ad4b9959851c977c657327f52b9a14dd94b2
Jules SANGLIER <jules.sanglier@gmail.com>
2021-02-07 22:40:41 +0100

    + ajout de la modification des éditeurs lors de la modification d'un livre

--- templates/t-modif-livre.php
+++ templates/t-modif-livre.php
@@ -3,8 +3,6 @@
     ob_start();
 ?>
 
-<h2>Blahblahblah</h2>
-
 <form method="post" action="controllers/c-modif-livre.php" id="modify-form">
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon1">#</span>
@@ -27,6 +25,38 @@
       <?php endforeach; ?>
     </select>
     <label class="input-group-text" for="inputGroupSelect02">Catégorie</label>
+  </div>
+
+  <div class="input-group mb-3">
+    <select class="form-select" id="inputGroupSelect02" name="editor">
+      <?php foreach($editeurs as $editeur): ?>
+        <?php if ($editeur['idEditeur'] == $livre['idEditeur']):?>
+          <option value="<?php echo $editeur['idEditeur']?>" selected><?php echo $editeur['nomEditeur'] ?></option>
+        <?php else: ?>
+            <option value="<?php echo $editeur['idEditeur']?>"><?php echo $editeur['nomEditeur'] ?></option>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    </select>
+    <label class="input-group-text" for="inputGroupSelect02">Éditeur</label>
+  </div>
+
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon2">Auteur(s)</span>
+    <ul class="list-group" style="max-height: 30vh; overflow: scroll;">
+      <?php foreach($auteurs as $auteur): ?>
+        <?php
+
+        ?>
+        <li class="list-group-item">
+          <?php if (in_array($auteur['idAuteur'], $auteurs_livre)): ?>
+          <input class="form-check-input me-1" name="auteurs-<?php echo $auteur['idAuteur']?>" type="checkbox" aria-label="..." checked>
+          <?php else: ?>
+          <input class="form-check-input me-1" name="auteurs-<?php echo $auteur['idAuteur']?>" type="checkbox" aria-label="...">
+          <?php endif; ?>
+          <?php echo $auteur['nomAuteur']. ' ' . $auteur['prenomAuteur']; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
   </div>
 
     <div class="col-12">
