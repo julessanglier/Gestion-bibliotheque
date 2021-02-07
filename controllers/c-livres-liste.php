1bb0a0f2847171185d78c19084cbb48e4f482a62
Jules SANGLIER <jules.sanglier@gmail.com>
2021-02-07 11:02:08 +0100

    + sys de multiples auteurs pour la liste de livres + prise en compte des breadcrumbs

--- controllers/c-livres-liste.php
+++ controllers/c-livres-liste.php
@@ -3,4 +3,7 @@ require_once "modele.php";
 
 $livres = get_livres();
 
+$breadcrumbs = Array(0 => Array("link" => "index.php", "page" => "Dashboard"),
+1 => Array("link" => "index.php?id=liste-livres", "page" => "Livres"));
+
 require "templates/t-liste-livres.php";
--- templates/t-liste-livres.php
+++ templates/t-liste-livres.php
@@ -3,13 +3,15 @@
     ob_start();
 ?>
 <div class="table-responsive">
+  <button type="button" class="btn btn-info" onclick="window.location.href = 'index.php?id=ajout-livre'">Ajouter un livre</a></button>
+  <button type="button" class="btn btn-info">Trier</button>
   <table class="table table-striped table-sm">
     <thead>
       <tr>
         <th>Reference</th>
         <th>Titre</th>
-        <th>Auteur</th>
-        <th>Collection</th>
+        <th>Auteur(s)</th>
+        <th>Catégorie</th>
         <th>Editeur</th>
         <th>Emprunté</th>
       </tr>
@@ -18,7 +20,7 @@
     <?php foreach ($livres as $livre): ?>
     <tr>
       <td>
-        <a href="modif_livre.php?id=">
+        <a href="index.php?id=modif-livre&lid= <?php echo $livre['idLivre'] ?>">
           <?php echo $livre['idLivre']; ?>
         </a>
       </td>
@@ -26,10 +28,17 @@
         <?php echo $livre['titreLivre']; ?>
       </td>
       <td>
-        <?php echo $livre['nomAuteur']; ?>
+        <?php
+        $auteurs = '';
+        $i = 0;
+        for ($i = 0; $i < sizeof($livre['auteursLivre']); $i++){
+            $auteurs .= $livre['auteursLivre'][$i]['nomAuteur'] . " " . $livre['auteursLivre'][$i]['prenomAuteur'] . ($i === (sizeof($livre['auteursLivre'])-1) ? "" : ", ");
+        }
+        echo $auteurs;
+        ?>
       </td>
       <td>
-        <?php echo $livre['nomCollection']; ?>
+        <?php echo $livre['nomCategorie']; ?>
       </td>
       <td>
         <?php echo $livre['nomEditeur']; ?>
