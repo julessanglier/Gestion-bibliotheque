23591d1dbf06bdd57aa909281c55f8627f07b535
Jules SANGLIER <jules.sanglier@gmail.com>
2021-02-07 22:35:59 +0100

    - graphe des emprunts + cards avec des stats

--- controllers/c-graphe-emprunts.php
+++ controllers/c-graphe-emprunts.php
@@ -1,5 +1,8 @@
 <?php
 
+$books = get_nb_livres();
+$books_reserved = get_nb_livres_reserves();
+$adherents = get_nb_adherents();
 $breadcrumbs = Array(0 => Array("link" => "index.php", "page" => "Dashboard"));
 
 require 'templates/t-graphe-emprunts.php';
--- templates/t-graphe-emprunts.php
+++ templates/t-graphe-emprunts.php
@@ -3,7 +3,33 @@
     ob_start();
 ?>
 
-<canvas class="my-4 m-100" id="myChart" width="900" height="380"></canvas>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card" style="margin-bottom: 10px;">
+      <div class="card-body">
+        <h5 class="card-title">Nombres de livres enregistrés</h5>
+        <p class="card-text"><?php echo $books->nbLivres ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card" style="margin-bottom: 10px;">
+      <div class="card-body">
+        <h5 class="card-title">Nombre de livres empruntés en ce moment</h5>
+        <p class="card-text"><?php echo $books_reserved->nbEmprunts ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card" style="margin-bottom: 10px;">
+      <div class="card-body">
+        <h5 class="card-title">Nombre d'adhérents à la Bibliothèque</h5>
+        <p class="card-text"><?php echo $adherents->nbAdh ?></p>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <?php
     // On récupère le contenu du buffer et on vide le buffer
